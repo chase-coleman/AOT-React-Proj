e6280f9916aad2b6555f591862c6af874f3026ae
YOUR_NAME <YOUR_EMAIL>
2025-03-13 21:44:41 -0500

    Changed branch name from master to main, changed proj-directory name from rickandmorty to aot-react, created pagination function to grab all aot characters from all pages of the API

--- src/pages/Characters.jsx
+++ src/pages/Characters.jsx
@@ -6,12 +6,14 @@ import getCharacters from "../utilities/getCharacters";
 //  use pagination to get calls for multiple pages
 
 const Characters = () => {
-  const { characters, setCharacters} = useOutletContext();
-  const pages = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+  const { characters, setCharacters } = useOutletContext();
+  const pages = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
   useEffect(() => {
     const fetchCharacters = async () => {
       const chars = await getCharacters(pages);
-      console.log(chars)
+      // console.log(chars)
+
+      // now we need to iterate through every "chars page", and add each character to the list
       setCharacters(chars);
     };
     fetchCharacters();
--- src/utilities/getCharacters.js
+++ src/utilities/getCharacters.js
@@ -2,12 +2,25 @@ import axios from "axios";
 import { data } from "react-router-dom";
 
 const getCharacters = async (pages) => {
-  console.log(pages)
+  const allCharacters = [];
   let responses = await Promise.all(
-    pages.map((page) => axios.get(`https://api.attackontitanapi.com/characters?page=${page}`)))
-  const data = responses.map((res) => res.data)
-  // console.log(data)
-  return data;
+    pages.map((page) =>
+      axios.get(`https://api.attackontitanapi.com/characters?page=${page}`)
+    )
+  );
+  const data = responses.map((res) => res.data);
+  console.log(data.results);
+  const characters = data;
+  // console.log(characters)
+  for (let obj of characters) {
+    for (let char of obj.results) {
+      allCharacters.push(char);
+      // console.log(char)
+    }
+  }
+  // console.log(allCharacters)
+
+  return allCharacters;
 };
 
 export default getCharacters;
